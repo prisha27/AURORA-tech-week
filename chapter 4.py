5ec50b04f492bd005e1754317f525f89b90eaba7
prisha27 <123857524+prisha27@users.noreply.github.com>
2023-02-12 12:11:07 +0530

    Delete chapter 4.py

--- chapter 4.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-img = cv2.imread("imagesrc/acm.jpg")
-hor = np.hstack((img,img))
-ver = np.vstack((img,img))
-cv2.imshow("horizontal image", hor)
-cv2.imshow("vertical stack", ver)
-cv2.waitKey(0)
-
